79ee347480eabe813e54b1310a2d41693e046cd0
e1982b <e1982b@gmail.com>
2023-03-21 11:17:54 +0100

    The files are uploaded

---
+++ src/Test_1.java
@@ -0,0 +1,8 @@
+public class Test_1 {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+        int a = 5, b = 6;
+        System.out.println(a + b);
+        System.out.println("a*b = " + a * b);
+    }
+}
---
+++ src/Test_2.java
@@ -0,0 +1,14 @@
+public class Test_2 {
+
+    int x = 15, y = 20;
+
+    public int sum() {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        Test_2 test= new Test_2();
+        System.out.println("test.sum() = " + test.sum());
+
+    }
+}
