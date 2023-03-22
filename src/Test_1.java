0b267fb4871a6fbb13920c3e7b01fa4bab52ee44
e1982b <e1982b@gmail.com>
2023-03-22 12:54:30 +0100

    The project is updated with bjk Branch (Test_1)

--- src/Test_1.java
+++ src/Test_1.java
@@ -1,13 +1,14 @@
 public class Test_1 {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        int a = 5, b = 6;
+        int a = 5, b = 6, c=10;
         System.out.println("sum of numbers =" + (a + b));
         System.out.println(a + b);
         // a+b toplami
         System.out.println("a*b = " + a * b);
         // a/b bölümü
         System.out.println(a / b);
+        System.out.println("(a*b)/c = " + (a * b) / c);
 
     }
 }
