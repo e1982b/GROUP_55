0c22cb56680e68d9edc1f32bd6c65f0d611c5e90
YahyaUygur <av.yahyauygur@gmail.com>
2023-03-22 10:54:23 +0100

    The project was updated

--- src/Test_1.java
+++ src/Test_1.java
@@ -1,9 +1,13 @@
 public class Test_1 {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("sdfgdf");
         int a = 5, b = 6;
+
         System.out.println(a + b);
+
         System.out.println("a*b = " + a * b);
+
         System.out.println(a/b);
     }
 }
