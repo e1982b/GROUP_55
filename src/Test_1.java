9be97eeb9ccf14ae777c0e9f576efef00e25a28e
Hamza <hamzasahinofficial@gmail.com>
2023-03-23 12:30:43 +0100

    Test1'e bir satır eklendi

--- src/Test_1.java
+++ src/Test_1.java
@@ -3,6 +3,7 @@ public class Test_1 {
         System.out.println("Merhaba");
         int a = 5, b = 6;
         System.out.println("sum of numbers =" + (a + b));
+        System.out.println(a+b);
         System.out.println(a + b);
         // a+b toplami
         System.out.println("a*b = " + a * b);
